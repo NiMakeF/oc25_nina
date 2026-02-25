e717576720825e761be2a0bfb9d1673af37688e7
NiMakeF <comichmocha856@gmail.com>
2026-02-25 10:52:30 +0100

    cg

--- 4_jeu/tutoriel2D/player.gd
+++ 4_jeu/tutoriel2D/player.gd
@@ -2,12 +2,15 @@ extends Area2D
 
 @export var speed = 400 
 var screen_size 
+signal hit
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 	
-func _process() :
-	var velocity = Vector2.ZERO 
+	
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -22,6 +25,34 @@ func _process() :
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
-		
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+	
+	if velocity.x < 0:
+		$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.flip_h = false
+
+
+
+
+
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+
+func _on_body_entered(body: Node2D) -> void:
+		hide() 
+		hit.emit()
+		$CollisionShape2D.set_deferred("disabled", true)
